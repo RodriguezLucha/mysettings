63889b2799259270ebd7b57883c958fb6ae65a30
rudy <rudy@opswat.com>
2017-10-04 13:37:36 -0700

    Updating some crap for windows mg but anyways will soon more to cygwin!

--- windows/aliases/mg.bat
+++ windows/aliases/mg.bat
@@ -1,2 +1,3 @@
 @echo off
-ag -i --nogroup -t --color-line-number 35 --color-match 31 --color-path 34 --path-to-ignore %~dp0.ignore %*
+REM ag -i --nogroup -t --color-line-number 35 --color-match 31 --color-path 34 --path-to-ignore %~dp0.ignore %*
+ag -i --nogroup -t --color-line-number 35 --color-match 31 --color-path 34 %*
