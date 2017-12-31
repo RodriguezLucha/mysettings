0ef2677b51e2aa5e98b83ccec5ff0e3a66638595
rudy <RodriguezLucha@gmail.com>
2017-12-31 12:22:22 -0800

    Adding web script to automatically update the hosts file

---
+++ linux/scripts/hosts_updater.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+from flask import Flask
+from flask import request
+import re
+app = Flask(__name__)
+
+
+@app.route('/')
+def update_hosts():
+    """
+    update the given hosts IP address in /etc/hosts
+    """
+    ip = request.args.get('ip')
+    hostname = request.args.get('hostname')
+    filename = "/etc/hosts"
+
+    old_lines = open(filename).read().splitlines()
+
+    new_lines = [
+        "{} {}".format(ip, hostname)
+        if re.search(hostname, x, re.IGNORECASE) else x for x in old_lines
+    ]
+
+    content = "\n".join(new_lines)
+
+    with open(filename, "w") as text_file:
+        text_file.write(content)
+
+    return content
+
+
+app.run(host='0.0.0.0')
